ed8191fbf3db9235a259b342969c804670c5bce9
faezeh <famo@di.ku.dk>
2022-05-16 10:28:09 +0200

    pubic_update

--- src/double_piece_joint.py
+++ src/double_piece_joint.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:afc513f8e1b215381b4a3dfb5fb208d42dec0ae00143c5d2d1387fd77d51fdd2
-size 33205
+oid sha256:6d3a128b6e884ed14b5b47eef536b4060762875cbad86deba12c441662526d94
+size 49558
--- src/single_piece_joint.py
+++ src/single_piece_joint.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:946a1c483767b55523dd3141deb6ba856157dbbf8c8903ff5333777b778f4c25
-size 33126
+oid sha256:953c83dd73de43fddadda19124d7c89c87ad8338741d60fc7aeedd81c5148bdf
+size 15331
